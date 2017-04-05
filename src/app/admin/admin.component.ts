fba01c08752839421f69b00377d9bcb5f202ac33
johnorobson <johnathan.g.robson@gmail.com>
2017-04-04 17:30:54 -0700

    removed the admin page and copied the authentication from the zenith assignment

--- src/app/admin/admin.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
-})
-export class AdminComponent {
-}
