6046f5342b456767691043fd74aaf4907661157e
johnorobson <johnathan.g.robson@gmail.com>
2017-04-06 17:42:41 -0700

    authentication is working

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
-import {Observable} from 'rxjs/Observable';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthenticationService implements CanActivate {
-    TOKEN_NAME: string = "zenithToken";
+    private TOKEN_NAME: string = "zenithToken";
+    private URL = 'http://dotnetbcbackend.azurewebsites.net/connect/token';
+
+    constructor(private http: Http) { }
+
+    authenticate(username: string, password: string): void {
+        let creds = 'username=' + username + '&password=' + password + '&grant_type=password'
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options = new RequestOptions({ headers: headers });
+        this.http.post(this.URL, creds, options)
+            .toPromise()
+            .then(r => {
+                let user = r.json();
+                this.setToken(user["access_token"]);
+            });
+    }
 
     getToken(): string {
-        let token:string = localStorage.getItem(this.TOKEN_NAME);
+        let token: string = localStorage.getItem(this.TOKEN_NAME);
 
         if (token == undefined) {
             return "";
@@ -16,11 +33,12 @@ export class AuthenticationService implements CanActivate {
         return token;
     }
 
-    setToken(token:string):void {
+    setToken(token: string): void {
         localStorage.setItem(this.TOKEN_NAME, token);
     }
 
-    isLoggedIn():boolean {
+    isLoggedIn(): boolean {
+        console.log(this.getToken().length > 0);
         return this.getToken().length > 0;
     }
 
@@ -29,9 +47,9 @@ export class AuthenticationService implements CanActivate {
     }
 
     canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean>|Promise<boolean>|boolean {
-    return this.isLoggedIn();
-  }
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean> | Promise<boolean> | boolean {
+        return this.isLoggedIn();
+    }
 }
