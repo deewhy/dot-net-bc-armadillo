55b9b57932041ce5090bfceb54444543426fba15
johnorobson <johnathan.g.robson@gmail.com>
2017-04-06 19:16:48 -0700

    logging in and out now reloads the events so the correct events are shown

--- src/app/homepage/eventviewer.component.ts
+++ src/app/homepage/eventviewer.component.ts
@@ -2,19 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 import { Event } from '../models/event';
 import { EventService } from '../services/event.service';
+import { Listener } from '../interfaces/listener';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'event-viewer',
   templateUrl: './eventviewer.component.html',
-  styleUrls: ['./eventviewer.component.css']
+  styleUrls: ['./eventviewer.component.css'],
 })
-export class EventViewerComponent implements OnInit {
+export class EventViewerComponent implements OnInit, Listener  {
 
     public events: Event[];
+
+    constructor(private eventService: EventService, private authenticationService: AuthenticationService) {
+        this.authenticationService.registerListener(this);
+    }
+
+    notify(): void {
+        this.ngOnInit();
+    }
 
     ngOnInit(): void {
         this.eventService.getEvents().then(e => this.events = e);
     }
-
-    constructor(private eventService: EventService) { }
 }
